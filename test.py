c3710b0524f16b43e2e83229619184527e8dfeb8
Gnvd21 <giodagoro@gmail.com>
2022-10-03 10:52:20 +0800

    edit

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 x = 10
 y = 3
 
-print(x/y)#x divided by y
-print(x//y)#x floor divided by y (division exclusing remainder)
-print(x*y)#x times y
-print(x**y)#x raised to the power of y
+#x divided by y
+print(x/y)
+#x floor divided by y (division exclusing remainder)
+print(x//y)
+#x times y
+print(x*y)
+#x raised to the power of y
+print(x**y)
