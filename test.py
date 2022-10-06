2a6d29aa7bb00674593ddc60b3bbc27329ed72e8
Gnvd21 <giodagoro@gmail.com>
2022-10-06 23:28:38 +0800

    add

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt, mpld3
+
 x = 10
 y = 3
 
@@ -9,3 +11,7 @@ print(x//y)
 print(x*y)
 #x raised to the power of y
 print(x**y)
+
+
+plt.plot([3,1,4,1,5], 'ks-', mec='w', mew=5, ms=20)
+mpld3.show()
